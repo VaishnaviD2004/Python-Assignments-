19ba090f4dd90578c24aeda985ed3fd8746874b4
VaishnaviD2004 <deshmanevaishnavi254@gmail.com>
2025-08-20 00:27:05 +0530

    U A1.py

--- A1.py
+++ A1.py
@@ -1,11 +1,7 @@
 # Python program to calculate sum of digits of a number
-
-# Taking input from user
 num = int(input("Enter a number: "))
-
 # Initialize sum
 sum_of_digits = 0
-
 # Loop to extract digits
 while num > 0:
     digit = num % 10      # get last digit
@@ -14,3 +10,4 @@ while num > 0:
 
 # Display result
 print("Sum of digits =", sum_of_digits)
+
